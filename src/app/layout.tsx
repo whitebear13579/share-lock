44e590f6eefccd6e0a173222c386a3649c1262ee
whitebear13579 <huangpeter0927@gmail.com>
2025-08-08 02:44:02 +0800

    add: 驗證元件

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import Script from "next/script";
 import "./globals.css";
 import { HeroUIProvider } from "@heroui/react";
+import { AuthProvider } from "@/utils/AuthContext";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,7 +27,9 @@ export default function RootLayout({
       </head>
       <body className="emfont-NotoSansTC">
         <HeroUIProvider>
-          {children}
+          <AuthProvider>
+            {children}
+          </AuthProvider>
         </HeroUIProvider>
       </body>
     </html>
