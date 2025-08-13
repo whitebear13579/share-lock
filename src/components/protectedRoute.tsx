9ad0b11f970b8bd54c693f71a030495f543b64a3
whitebear13579 <huangpeter0927@gmail.com>
2025-08-14 01:44:11 +0800

    fix: 移除 protectedRoute 元件對於isLoading 狀態的處理

--- src/components/protectedRoute.tsx
+++ src/components/protectedRoute.tsx
@@ -31,13 +31,13 @@ export default function ProtectedRoute({
     }
   }, [user, loading, requireAuth, redirectTo, router]);
 
-  if (loading || forceLoading) {
+  /*if (loading || forceLoading) {
     return (
       <div className="min-h-screen bg-neutral-800 flex items-center justify-center">
         <Spinner classNames={{label: "text-xl text-white"}} variant="dots" size='lg' color='default' label='載入中...'/>
       </div>
     );
-  }
+  }*/
 
   if (requireAuth && !user) {
     return null;
