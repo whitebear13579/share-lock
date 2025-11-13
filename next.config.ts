6513eed7fc5f283b1b11d27f6ba053279b00ecef
whitebear13579 <huangpeter0927@gmail.com>
2025-11-13 13:10:36 +0800

    add: COOP header

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,19 @@ const nextConfig: NextConfig = {
         maxInactiveAge: 25 * 1000,
         pagesBufferLength: 2,
     },
+    async headers() {
+        return [
+            {
+                source: '/:path*',
+                headers: [
+                    {
+                        key: 'Cross-Origin-Opener-Policy',
+                        value: 'same-origin-allow-popups',
+                    },
+                ],
+            },
+        ];
+    },
 };
 
 export default nextConfig;
